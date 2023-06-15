c918478cbe778159ce3d1606e7dc474a38aa08e9
wakai-megumi <tarun483sharma@gmail.com>
2023-06-15 22:47:02 +0530

    handling the catch all route

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import connectDB from './database.js';
 import express from 'express';
 import path from 'path';
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.SERVER_PORT || 3000;
