74dc805c371f4044060316faaa4fc9fd95672df6
wakai-megumi <tarun483sharma@gmail.com>
2023-06-15 17:41:28 +0530

    added origin

--- index.js
+++ index.js
@@ -1,57 +1,58 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import hotelRouter from './routers/hotel.route.js';
-import userRouter from './routers/user.route.js';
-import authRouter from './routers/auth.route.js';
-import roomRouter from './routers/rooms.route.js';
-import bookingrouter from './routers/booking.route.js';
+import express from 'express'
+import dotenv from "dotenv"
+import hotelRouter from './routers/hotel.route.js'
+import userRouter from './routers/user.route.js'
+import authRouter from './routers/auth.route.js'
+import roomRouter from './routers/rooms.route.js'
+import bookingrouter from './routers/booking.route.js'
 
-import cookieParser from 'cookie-parser';
-import ErrorHandler from './utils/ErrorHandler.js';
-
-dotenv.config();
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import ErrorHandler from './utils/ErrorHandler.js'
+dotenv.config()
 
 export const app = express();
+app.use(cookieParser())
 
-app.use(cookieParser());
 
-app.use((req, res, next) => {
-    const allowedOrigins = [
-        'https://waneem-admin.onrender.com',
-        'https://waneem.onrender.com',
-    ];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+//middleware
+app.use(cors({
+    origin: "https://waneem-admin.onrender.com",
+    credentials: true,
+    optionSuccessStatus: true,
+    methods: "*"
+}))
+app.set("trust proxy", 1)
+app.use(express.json())
+//router
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/hotels', hotelRouter)
+app.use('/api/v1/room', roomRouter)
+app.use('/api/v1/booking', bookingrouter)
 
-app.use(express.json());
 
-// Routers
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/hotels', hotelRouter);
-app.use('/api/v1/room', roomRouter);
-app.use('/api/v1/booking', bookingrouter);
-app.use('/api/v1/user', userRouter);
+app.use('/api/v1/user', userRouter)
 
-const token = 'Sdfsdfsd';
-app.get('/api/v1', (req, res) => {
-    res
-        .status(200)
-        .cookie('access_token', token, {
+const toke = "Sdfsdfsd"
+app.get(
+    '/api/v1', (req, res) => {
+        console.log(toke)
+        console.log('here')
+        res.status(200).cookie("access_token", toke, {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
-            sameSite: 'none',
-            secure: true,
-        })
-        .send('hello from express');
-});
+            sameSite: "none",
+            secure: true
+        }).send('hello from express')
+        console.log(res)
+    }
 
-// Error middleware
-app.use(ErrorHandler);
+
+)
+console.log(process.env.ADMIN_URL, process.env.CLIENT_URL)
+
+
+//error middleware
+
+app.use(ErrorHandler)
 
