ae35a86b5a82266ebfc79954a6d68a1459a3cd32
wakai-megumi <tarun483sharma@gmail.com>
2023-06-15 14:27:42 +0530

    cook

--- utils/Sendtoken.js
+++ utils/Sendtoken.js
@@ -10,7 +10,7 @@ export const Sendtoken = async (user, res, statusCode = 200, message, next) => {
         return res.status(statusCode).cookie("access_token", token, {
             httpOnly: true,
             expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-            // sameSite: "none",
+            sameSite: "Strict",
             // secure: true
         }).json({
             success: true,
